aed0000f8af1efa5edcfaf1b300b7d33b90e1295
WD0LB <58177306+WD0LB@users.noreply.github.com>
2021-11-14 20:17:19 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,28 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
-import csv
+from flask import Flask, request
 app = Flask(__name__)
-
-def write_to_file_stl(data):
-	with open('database_stl.txt', mode='a') as database:
-		device = data['device']
-		date = data['date']
-		comment = data['comment']
-		file = database.write(f'\n{device}, {date}, {comment}')
-def write_to_csv_stl(data):
-	with open('database_stl.csv', mode='a', newline='') as database:
-		device = data['device']
-		date = data['date']
-		comment = data['comment']
-		csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		csv_writer.writerow([device,date,comment])
 
 @app.route('/stl', methods=['POST','GET'])
 def stl():
 	if request.method == 'POST':
 		try:
 			data = request.form.to_dict()
-			write_to_file_stl(data)
-			write_to_csv_stl(data)
+			print(data)
 		except :
 			return 'something went wrong'
 		return 'stl connection success'
